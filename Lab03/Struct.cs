be8f53145321b1e8c92613381783a49b3aa0883d
AndreyPapchinsky <Ha.xJle6ywek@yandex.ru>
2021-11-21 23:11:21 +0300

    Добавьте файлы проекта.

---
+++ Lab03/Struct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab03
+{
+    public struct Distance
+    {
+        public int Feet;
+        public int Inches;
+    }
+    class Struct
+    {
+        static void Main(string[] args)
+        {
+            Distance vOne = new ();
+            Distance vTwo = new ();
+            Distance vSum = new ();
+
+            Console.WriteLine("Введите значение в дюймах для первой переменной: ");
+            vOne.Inches = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение в дюймах для второй переменной: ");
+            vTwo.Inches = int.Parse(Console.ReadLine());
+
+            vSum.Feet = (vOne.Inches + vTwo.Inches) /12;
+            vSum.Inches = (vOne.Inches + vTwo.Inches) % 12;
+
+            Console.WriteLine(vSum.Feet + "-" + vSum.Inches);
+        }
+    }
+}
